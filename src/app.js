2ceaeb1522c90a0da406e3b466275f80231443ab
alejandrojmnzz <alejandro12850mj@gmail.com>
2024-10-18 22:03:10 +0000

    random card generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,30 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let symbols = ["♦", "♥", "♠", "♣"];
+let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let card = document.querySelector(".card");
+
+let cardTop = document.createElement("div");
+let cardMid = document.createElement("div");
+let cardBottom = document.createElement("div");
+
+card.appendChild(cardTop).classList = "symbols";
+card.appendChild(cardMid);
+card.appendChild(cardBottom).classList = "symbols";
+
+let randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+
+cardTop.innerHTML = `<span>${randomSymbol}<span/>`;
+
+cardBottom.innerHTML = `<span>${randomSymbol}<span/>`;
+cardBottom.style.justifyContent = "end";
+cardBottom.children[0].style.rotate = "180deg";
+
+if (randomSymbol == symbols[0] || randomSymbol == symbols[1]) {
+  card.style.color = "red";
+}
+
+cardMid.textContent = values[Math.floor(Math.random() * values.length)];
+cardMid.classList = "value";
